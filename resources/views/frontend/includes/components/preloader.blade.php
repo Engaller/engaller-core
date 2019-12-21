9e8fab8d67b68f26a0d85b882735cf638311b136
Sanjaya Harshana <sanjayaharasana@gmail.com>
2019-12-21 19:29:53 +0530

    Preloader ASY Script

---
+++ resources/views/frontend/includes/components/preloader.blade.php
@@ -0,0 +1,9 @@
+<body>
+<!--preloading-->
+<div id="preloader">
+    <img class="logo" src="images/logo1.png" alt="" width="119" height="58">
+    <div id="status">
+        <span></span>
+        <span></span>
+    </div>
+</div>
