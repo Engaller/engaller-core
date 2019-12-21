7353e3bd10e6b6bba6c7585be1b0d9100523f151
Anupama Dikkubura <anupamadikkubura@gmail.com>
2019-12-21 19:58:32 +0530

    search comp created

---
+++ resources/views/frontend/includes/components/search.blade.php
@@ -0,0 +1,7 @@
+{{-- <div class="top-search">
+    <select>
+        <option value="united">TV show</option>
+        <option value="saab">Others</option>
+    </select>
+    <input type="text" placeholder="Search for a movie, TV Show or celebrity that you are looking for">
+</div> --}}
